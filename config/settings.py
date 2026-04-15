df9565cdaa5154992364b3216b49080d241f6af2
ialeks017 <108348616+ialeks017@users.noreply.github.com>
2026-04-15 23:36:37 +0300

    Add custom user model with role choices

--- config/settings.py
+++ config/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -71,3 +72,5 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
